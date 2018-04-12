3997f2532dda35275c0dfc6c32226c2130ef5682
EOussama <oussama_gtacj@live.fr>
2018-04-12 21:08:34 +0100

    Basic buttons and calculations
    - All basic operators are working decently fine.
    - README.md added.

--- Calculator/MainWindow.xaml.vb
+++ Calculator/MainWindow.xaml.vb
@@ -1,3 +1,151 @@
 ﻿Class MainWindow
+    Dim active As Integer
+    Dim values(1) As Double
+    Dim optors As Char
 
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        CE_Click(sender, e)
+    End Sub
+
+    Private Sub Calcul()
+        Try
+            Select Case optors
+                Case "+"
+                    values(1) = values(0) + values(1)
+                    resCreen.Text = values(1)
+                Case "-"
+                    values(1) = values(0) - values(1)
+                    resCreen.Text = values(1)
+                Case "*"
+                    values(1) = values(0) * values(1)
+                    resCreen.Text = values(1)
+                Case "/"
+                    values(1) = values(0) / values(1)
+                    resCreen.Text = values(1)
+            End Select
+        Catch ex As Exception
+            MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error)
+        End Try
+    End Sub
+
+    Private Sub CE_Click(sender As Object, e As RoutedEventArgs) Handles CE.Click
+        active = 0
+        values = {0, 0}
+        optors = "+"
+
+        resCreen.Text = 0
+    End Sub
+
+    Private Sub C_Click(sender As Object, e As RoutedEventArgs) Handles C.Click
+        values(active) = 0
+        resCreen.Text = 0
+    End Sub
+
+    Private Sub num0_Click(sender As Object, e As RoutedEventArgs) Handles num0.Click
+        If resCreen.Text.Length <= 10 Then
+            values(active) = Double.Parse(resCreen.Text + "0")
+            resCreen.Text = values(active)
+        End If
+    End Sub
+
+    Private Sub num1_Click(sender As Object, e As RoutedEventArgs) Handles num1.Click
+        If resCreen.Text.Length <= 10 Then
+            values(active) = Double.Parse(resCreen.Text + "1")
+            resCreen.Text = values(active)
+        End If
+    End Sub
+
+    Private Sub num2_Click(sender As Object, e As RoutedEventArgs) Handles num2.Click
+        If resCreen.Text.Length <= 10 Then
+            values(active) = Double.Parse(resCreen.Text + "2")
+            resCreen.Text = values(active)
+        End If
+    End Sub
+
+    Private Sub num3_Click(sender As Object, e As RoutedEventArgs) Handles num3.Click
+        If resCreen.Text.Length <= 10 Then
+            values(active) = Double.Parse(resCreen.Text + "3")
+            resCreen.Text = values(active)
+        End If
+    End Sub
+
+    Private Sub num4_Click(sender As Object, e As RoutedEventArgs) Handles num4.Click
+        If resCreen.Text.Length <= 10 Then
+            values(active) = Double.Parse(resCreen.Text + "4")
+            resCreen.Text = values(active)
+        End If
+    End Sub
+
+    Private Sub num5_Click(sender As Object, e As RoutedEventArgs) Handles num5.Click
+        If resCreen.Text.Length <= 10 Then
+            values(active) = Double.Parse(resCreen.Text + "5")
+            resCreen.Text = values(active)
+        End If
+    End Sub
+
+    Private Sub num6_Click(sender As Object, e As RoutedEventArgs) Handles num6.Click
+        If resCreen.Text.Length <= 10 Then
+            values(active) = Double.Parse(resCreen.Text + "6")
+            resCreen.Text = values(active)
+        End If
+    End Sub
+
+    Private Sub num7_Click(sender As Object, e As RoutedEventArgs) Handles num7.Click
+        If resCreen.Text.Length <= 10 Then
+            values(active) = Double.Parse(resCreen.Text + "7")
+            resCreen.Text = values(active)
+        End If
+    End Sub
+
+    Private Sub num8_Click(sender As Object, e As RoutedEventArgs) Handles num8.Click
+        If resCreen.Text.Length <= 10 Then
+            values(active) = Double.Parse(resCreen.Text + "8")
+            resCreen.Text = values(active)
+        End If
+    End Sub
+
+    Private Sub num9_Click(sender As Object, e As RoutedEventArgs) Handles num9.Click
+        If resCreen.Text.Length <= 10 Then
+            values(active) = Double.Parse(resCreen.Text + "9")
+            resCreen.Text = values(active)
+        End If
+    End Sub
+
+    Private Sub numDec_Click(sender As Object, e As RoutedEventArgs) Handles numDec.Click
+        If Not resCreen.Text.Contains(".") Then
+            resCreen.Text &= "."
+        End If
+    End Sub
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Call Calcul()
+    End Sub
+
+    Private Sub optPlus(sender As Object, e As RoutedEventArgs)
+        optors = "+"
+        values(active) = Double.Parse(resCreen.Text)
+        active = If(active = 0, 1, 0)
+        resCreen.Text = 0
+    End Sub
+
+    Private Sub optMin_Click(sender As Object, e As RoutedEventArgs) Handles optMin.Click
+        optors = "-"
+        values(active) = Double.Parse(resCreen.Text)
+        active = If(active = 0, 1, 0)
+        resCreen.Text = 0
+    End Sub
+
+    Private Sub optMul_Click(sender As Object, e As RoutedEventArgs) Handles optMul.Click
+        optors = "*"
+        values(active) = Double.Parse(resCreen.Text)
+        active = If(active = 0, 1, 0)
+        resCreen.Text = 0
+    End Sub
+
+    Private Sub optDiv_Click(sender As Object, e As RoutedEventArgs) Handles optDiv.Click
+        optors = "/"
+        values(active) = Double.Parse(resCreen.Text)
+        active = If(active = 0, 1, 0)
+        resCreen.Text = 0
+    End Sub
 End Class
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Calculator
-A simple WPF calculator just for the giggles!
+---
+
+## Description
+Not the most impressive beginner project I know, I just got my head around **Visual Basic**, and I needed to do something to test what I knowso far, I worked on a web browser, and this is another experimental project to shut my doubts around this language.
+I'm aiming to create a simple calculator thatdoes theabsolute basics, (_Add, substract, multiply, and devide_).
+In addition to my **VB** training, this is my absolute first approach to **WPF** forms, I guess I need to wrap my head up aroundthis **XAML** thing asap!
